ca75e974598ec22efbedd30fec185cb9dc79d919
cenation007 <ayushpandey007bond@gmail.com>
2020-05-02 13:14:00 +0530

    'Added_initial_stuff'

---
+++ rbac-tutorial-app/src/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    domain: "dev-0w4jhm39.auth0.com",
+    roleUrl: "https://rbac-tutorial-app/role",
+    clientId: "PFsxUouEWqCxxmkUhGkDKAhLhESNZsmV",
+    callbackUrl: "http://localhost:3000/callback",
+};
