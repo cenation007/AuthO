1402af43e6132699c0c43e500bb6f629b27a0bb0
Ayush Pandey <44978304+cenation007@users.noreply.github.com>
2020-08-31 23:45:02 +0530

    Update auth0-variables.js

--- react_app/src/auth0-variables.js
+++ react_app/src/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
     domain: 'dev-fs64ifi5.auth0.com',
-    roleUrl: 'https://rbac-tutorial-app/role',
+    roleUrl: 'https://rbac-app/role',
     clientId: 'PX63SgCy8WyWYNsk7Dlcrwqd7pM1mCdX',
     callbackUrl: 'http://localhost:3000/callback',
 };
